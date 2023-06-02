0b0bf85d68af6d57e83afeb76a13cc6a3d7f4634
Kreyno93 <michael.wagener@neuefische.de>
2023-06-02 17:27:04 +0200

    wichtige sysouts

--- backend/src/main/java/de/neuefische/backend/BackendApplication.java
+++ backend/src/main/java/de/neuefische/backend/BackendApplication.java
@@ -8,7 +8,17 @@ public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-        System.out.println("test");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
+        System.out.println("BackendApplication started");
 
     }
 
